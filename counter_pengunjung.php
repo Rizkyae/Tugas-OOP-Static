dbf7a67c37a1729adcd8d9e7b434e94a6adacb54
Rizkyae <rizkyamirullah245@gmail.com>
2026-04-22 18:43:22 +0700

    Praktikum 1: Versi After (5 objek + reset)

--- counter_pengunjung.php
+++ counter_pengunjung.php
@@ -1,7 +1,27 @@
 <?php
+class Pengunjung {
+    public static $jumlah = 0;
 
+    public function __construct() {
+        self::$jumlah++;
+    }
+
+    // Tambahkan method reset ini [cite: 224]
+    public static function reset() {
+        self::$jumlah = 0; [cite: 227]
+    }
+}
+
+// Ubah jadi 5 objek 
 $p1 = new Pengunjung();
 $p2 = new Pengunjung();
 $p3 = new Pengunjung();
+$p4 = new Pengunjung();
+$p5 = new Pengunjung();
 
+echo "Jumlah Pengunjung Sebelum Reset: " . Pengunjung::$jumlah . "<br>"; // Muncul 5
+
+Pengunjung::reset(); // Panggil fungsi reset [cite: 237]
+
+echo "Jumlah Pengunjung Setelah Reset: " . Pengunjung::$jumlah; // Muncul 0
 ?>
